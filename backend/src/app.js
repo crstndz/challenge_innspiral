4d3625687f17c2df93daf7e49ec3347fca75dac6
crstndz <cristianf.diaz@gmail.com>
2021-02-24 00:03:31 -0300

    Add swagger documentation

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const morgan = require("morgan");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger/swagger.json");
 const app = express();
 
 //settings
@@ -9,8 +11,9 @@ app.set("port", process.env.PORT || 4000);
 
 //middlewares
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 app.use(morgan("dev"));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 //routes
 app.use("/api/clientes", require("./routes/clientes"));
